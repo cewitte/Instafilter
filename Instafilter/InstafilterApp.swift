2ca1d08ef48c7327c533030906c7b7e635bf76ad
Carlos Eduardo Witte <cewitte@icloud.com>
2025-04-09 08:01:05 -0300

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Carlos Eduardo Witte on 09/04/25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
